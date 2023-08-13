09546cb17baa9368082801258a6d0d1c9eef6ddc
akumakumu <akumakumu@gmail.com>
2023-08-13 23:31:02 +0700

    fix tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,rs}",
-    "./index.html"
-  ],
+  content: ["./src/**/*.{html,rs}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
